a69e8b9448a2a9e94047357a6f1de2d65636afa5
JoseLeonardoCarrilloRamirez <joseleonardocarrillo@gmail.com>
2021-07-17 16:00:16 -0500

    operadores de comparacion

---
+++ main.js
@@ -0,0 +1,30 @@
+//Pedir 2 datos por pantalla y almacenarlo en dos variables 
+//diferentes:
+//Variable 1(8)  Variable 2 (20)
+//Realizar todas las Operacion compracion vistas en la clase
+//Mostra en la consola todos los posibles resultados
+//y explicar con sus propias palabras porque se dio es resultado 'true , false'
+
+let numero1 = 8;
+let numero2 = 20;
+
+console.log(`El numero1 es:`+numero1);
+console.log(`El numero2 es:`+numero2);
+
+//Operador Mayor que
+console.log(`El numero ${numero1} es mayor que el numero ${numero2} :`,numero1>numero2);
+//Operador Mayor o igual 
+console.log(`El numero ${numero1} es mayor o igual que el numero ${numero2} :`,numero1>=numero2);
+//Operador Menor que
+console.log(`El numero ${numero1} es menor que el numero ${numero2} :`,numero1<numero2);
+//Operador Menor o igual 
+console.log(`El numero ${numero1} es menor o igual que el numero ${numero2} :`,numero1<=numero2);
+
+//Operador Igual que
+console.log(`El numero ${numero1} es igual que el numero ${numero2} :`,numero1==numero2);
+//Operador Identico que 
+console.log(`El numero ${numero1} ${typeof numero1} es identico que el numero ${numero2} ${typeof numero2} :`,numero1===numero2);
+//Operador Diferente que 
+console.log(`El numero ${numero1} es diferente que el numero ${numero2} :`,numero1!=numero2);
+//Operador no Diferente que 
+console.log(`El numero ${numero1} es diferente que el numero ${numero2} :`,numero1!==numero2);
